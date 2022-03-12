f0f2607b102c047f1084ba4d383879ae94024664
Ren <ren.t21@icloud.com>
2022-03-13 00:48:08 +0900

    comp

--- weatherViewer/ViewController.swift
+++ weatherViewer/ViewController.swift
@@ -4,68 +4,70 @@
 //
 //  Created by 高橋蓮 on 2022/03/12.
 //
-
-
 import UIKit
 import Alamofire
-import CoreLocation
-import NVActivityIndicatorView
 import SwiftyJSON
+import NVActivityIndicatorView
+import CoreLocation
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
-    
-    @IBOutlet weak var DateLabel: UILabel!
-    @IBOutlet weak var WeatherImage: UIImageView!
-    @IBOutlet weak var LocationLabel: UILabel!
-    @IBOutlet weak var templabel: UILabel!
-    @IBOutlet weak var weatherLabel: UILabel!
-    
-    let gradientLayer = CAGradientLayer()
-    let apikey = "e314c7e4533ae54e9740790e14b56ac3"
-    var lat = 11.34553
-    var lon = 10.3344
+
+
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var conditionLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+
+
+    let apiKey = "e314c7e4533ae54e9740790e14b56ac3"
+    var lat = 26.8205
+    var lon = 30.8024
+    // loading
     var activityIndicator: NVActivityIndicatorView!
+    // user location
     let locationManager = CLLocationManager()
-    
+
+
     override func viewDidLoad() {
-        view.layer.addSublayer(gradientLayer)
-        let indicateSize: CGFloat = 70
-        let indicateFrame = CGRect(x: (view.frame.width-indicateSize) / 2, y: (view.frame.height-indicateSize) / 2, width: indicateSize, height: indicateSize)
-        activityIndicator = NVActivityIndicatorView(frame: indicateFrame, type: .lineScale, color: UIColor.white, padding: 20.0)
+        super.viewDidLoad()
+
+        let indicatorSize: CGFloat = 70
+        let indicatorFrame = CGRect(x: (view.frame.width-indicatorSize)/2, y: (view.frame.height-indicatorSize)/2, width: indicatorSize, height: indicatorSize)
+        activityIndicator = NVActivityIndicatorView(frame: indicatorFrame, type: .lineScale, color: UIColor.white, padding: 20.0)
+        activityIndicator.backgroundColor = UIColor.black
         view.addSubview(activityIndicator)
+
+        // use popup to check and get location
         locationManager.requestWhenInUseAuthorization()
-        activityIndicator.startAnimating()
-        if(CLLocationManager.locationServicesEnabled()) {
+        if (CLLocationManager.locationServicesEnabled()) {
             locationManager.delegate = self
             locationManager.desiredAccuracy = kCLLocationAccuracyBest
             locationManager.startUpdatingLocation()
         }
     }
-    override func viewWillAppear(_ animated: Bool) {
-        blueGragiantBockGround()
-    }
-    
+
+
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let location = locations[0]
+        print(location)
         lat = location.coordinate.latitude
         lon = location.coordinate.longitude
-        AF.request("http://api.openweathermap.org/data/2.5/weather?lat=\(lat)&lon=\(lon)&appaid=\(apikey)&units=metric").responseJSON { response in
+
+        AF.request("https://api.openweathermap.org/data/2.5/weather?lat=\(lat)&lon=\(lon)&appid=\(apiKey)&units=metric").responseJSON { response in
             self.activityIndicator.stopAnimating()
             if let responseStr = response.value {
                 let jsonResponse = JSON(responseStr)
                 let jsonWeather = jsonResponse["weather"].array![0]
                 let jsonTemp = jsonResponse["main"]
                 let iconName = jsonWeather["icon"].stringValue
+
+                self.locationLabel.text = jsonResponse["name"].stringValue
+                self.conditionImageView.image = UIImage(named: iconName)
+                self.conditionLabel.text = jsonWeather["main"].stringValue
+                self.temperatureLabel.text = "\(Int(round(jsonTemp["temp"].doubleValue)))"
             }
         }
     }
-    
-    func blueGragiantBockGround() {
-        let topColor = UIColor(red: 95.0/255.0, green: 100.0/255.0, blue: 0.5, alpha: 1.0).cgColor
-        let ButtonColor = UIColor(red: 20.0/255.0, green: 114.0/255.0, blue: 211.1/255.0, alpha: 1.0).cgColor
-        gradientLayer.frame = view.bounds
-        gradientLayer.colors = [topColor, ButtonColor]
-    }
 }
 
-//https://www.youtube.com/watch?v=WHRntPeAOo4
